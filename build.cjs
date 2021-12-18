b5b5b62d5c942c1b1ff37b6ceaacf5fe18bc8141
Will Crichton <wcrichto@cs.stanford.edu>
2021-12-17 19:00:05 -0800

    Initial integration with nota-editor

--- build.cjs
+++ build.cjs
@@ -1,13 +1,18 @@
 const estrella = require("estrella");
 const { sassPlugin } = require("esbuild-sass-plugin");
 const fs = require("fs/promises");
+const glob = require("glob-promise");
+const path = require("path");
 
-estrella.build({
-  entry: "src/index.tsx",
-  outdir: "dist",
-  bundle: true,
-  plugins: [sassPlugin()]
-}).then(async () => {
+estrella
+  .build({
+    entry: "src/index.tsx",
+    outdir: "dist",
+    bundle: true,
+    preserveSymlinks: true,
+    plugins: [sassPlugin()],
+  })
+  .then(async () => {
     let index_html = `<!DOCTYPE html>
   <html>
     <head>
@@ -23,4 +28,11 @@ estrella.build({
   </html>`;
 
     await fs.writeFile("dist/index.html", index_html);
-  });;
+
+    let wasm = await glob(
+      "node_modules/@wcrichto/nota-editor/dist/components/*.wasm"
+    );
+    await Promise.all(
+      wasm.map((p) => fs.copyFile(p, path.join("dist", path.basename(p))))
+    );
+  });
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
+import { observer } from "mobx-react";
+import {
+  Editor,
+  OutputView,
+  JsView,
+  LocalState,
+  StateContext,
+} from "@wcrichto/nota-editor";
 
 import "../css/app.scss";
 
-let App = () => {
-  return <div>
-    <h1>Nota: A Document Language for the Browser</h1>
-    <p>This page is under construction!</p>
-  </div>
-};
+let contents = `
+@strong{Nota} is a language for writing documents.
+`.trim();
+
+let App = observer(() => {
+  let [state] = useState(() => {
+    let state = new LocalState();
+    state.contents = contents;
+    return state;
+  });
+
+  return (
+    <div>
+      <h1>Nota: A Document Language for the Browser</h1>
+      <div>
+        <StateContext.Provider value={state}>
+          {state.ready ? (
+            <>
+              <Editor />
+              <OutputView result={state.translation} />
+            </>
+          ) : (
+            "Loading..."
+          )}
+        </StateContext.Provider>
+      </div>
+    </div>
+  );
+});
 
 ReactDOM.render(<App />, document.getElementById("container"));

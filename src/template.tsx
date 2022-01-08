e2eb898d19c71182daee4df3d5e33c9a87c79237
Will Crichton <wcrichto@cs.stanford.edu>
2022-01-07 16:39:48 -0800

    Improve meta tags

--- src/template.tsx
+++ src/template.tsx
@@ -9,8 +9,8 @@ let Template: React.FC<TemplateProps> = ({ title, script, children }) => (
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta property="og:title" content={title} />
       <meta property="og:type" content="article" />
-      <meta property="og:description" content="Making a world where you never have to use LaTeX again" />
-      <meta property="og:image" content="/android-chrome-512x512.png" />
+      <meta property="og:description" content="Making a world where you never have to use LaTeX again." />
+      <meta property="og:image" content="https://nota-lang.org/android-chrome-512x512.png" />
       <meta name="twitter:card" content="summary_large_image" />
       <title>{title}</title>
     </head>
